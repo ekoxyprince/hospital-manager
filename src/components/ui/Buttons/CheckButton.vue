dec852583a78ce7c4730735708fc6f031917b365
Archimedes-Einstein <ekejiubaarchimedes@gmail.com>
2024-11-23 21:41:24 +0100

    changes

--- src/components/ui/Buttons/CheckButton.vue
+++ src/components/ui/Buttons/CheckButton.vue
@@ -1,59 +1,43 @@
 <template>
   <label class="custom-checkbox">
     <input type="checkbox" />
-    <span class="checkmark"></span>
+    <span class="pi pi-check checkbox"></span>
   </label>
 </template>
 
 <script>
 export default {};
 </script>
-<style>
+<style scoped>
 .custom-checkbox {
-  display: inline-flex;
+  display: flex;
   align-items: center;
+  justify-content: center;
+  border: 2px solid grey;
+  height: 1.2rem;
+  width: 1.2rem;
+  border-radius: 5px;
 }
 
-.custom-checkbox input {
+input {
   display: none; /* Hide the default checkbox */
 }
 
-.custom-checkbox .checkmark {
-  width: 20px;
-  height: 20px;
-  border: 2px solid #ccc; /* Border color */
-  border-radius: 4px; /* Rounded corners */
-  position: relative;
-  margin-right: 8px;
+.custom-checkbox .checkbox {
+  font-size: 0.6rem;
+  color: white;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.custom-checkbox input:checked + .checkmark {
+.custom-checkbox input:checked + .checkbox {
   background-color: rgb(22, 138, 216); /* Background color when checked */
   border-color: rgb(33, 131, 211); /* Border color when checked */
 }
-
-.custom-checkbox .checkmark::after {
-  content: "";
-  position: absolute;
-  display: none;
-}
-
-.custom-checkbox input:checked + .checkmark::after {
-  display: block;
-}
-
-.custom-checkbox .checkmark::after {
-  left: 7px;
-  top: 3px;
-  width: 5px;
-  height: 10px;
-  border: solid white; /* Checkmark color */
-  border-width: 0 2px 2px 0;
-  transform: rotate(45deg);
-}
-
-.custom-checkbox {
-  display: inline-flex;
-  align-items: center;
+.custom-checkbox input:checked {
+  border-color: rgb(22, 138, 216);
 }
 </style>
